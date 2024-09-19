e31e17ea99bdec8ba54c79d3ab4827e155b1db02
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-09-19 15:55:42 +0300

    Refactoring Server Architecture:
    - put the server connections and configurations into `index.js`.
    - delete `server.js` file.
    - modify node.js start command for deployment.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({
+  path: './.env'
+});
 
 const authRouter = require('./routes/authRoutes');
 
@@ -18,6 +24,35 @@ app.use('/api/auth', authRouter);
 app.get('/' , (req, res) => {
   res.status(200).send('Testing Connection...');
 }); 
+
+
+
+const port = process.env.PORT;
+let url ='';
+if (process.env.NODE_ENV === 'production') {
+  url = process.env.CLOUD_DB.replace('<db_password>', process.env.PASSWORD_DB);
+}else {
+  url = process.env.LOCAL_DB;
+}
+
+
+mongoose.set("strictQuery" , false);
+mongoose
+  .connect(url)
+  .then(() => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log('LOCAL DATABASE Connected Successfully!');
+    }else {
+      console.log('CLOUD DATABASE Connected Successfully!')
+    }
+    app.listen(port , () => {
+      console.log(`Learnify Server Liseneing on port ${port}...`);
+    });
+  })
+  .catch((error) => {
+    console.log(`MONGODB ERROR:\n${error}`);
+  });
+
 
 module.exports = {
   app
